69549169d8022e99c5bc077b22b82d86408a65da
nathan00pdl <nathanpaiva00@gmail.com>
2025-02-22 16:42:31 -0300

    refactoring controller

--- src/main/java/com/nathan/hello_world/controller/HelloWorldController.java
+++ src/main/java/com/nathan/hello_world/controller/HelloWorldController.java
@@ -1,11 +1,14 @@
 package com.nathan.hello_world.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
+@RestController
 public class HelloWorldController {
-    public static void main(String[] args) {
-        SpringApplication.run(HelloWorldController.class, args);
+    
+    @GetMapping("/api/hello")
+    public String sendHello() {
+        return "Hello World!";
     }
 }

8cc257566360d3f3246a0b685729da177839410a
nathan00pdl <nathanpaiva00@gmail.com>
2025-02-22 19:01:00 -0300

    test class refactoring

--- src/test/java/com/nathan/hello_world/controller/HelloWorldControllerTest.java
+++ src/test/java/com/nathan/hello_world/controller/HelloWorldControllerTest.java
@@ -12,20 +12,25 @@ import org.springframework.test.web.servlet.MockMvc;
 
 /*
  * MockMvc:
- * 
  * -> Simulation of an HTTP request to /api/hello endpoint.
  * -> Checking the response status.
  * -> Checking the content of the response.  
+ * 
+ * JUnit5:
+ * -> Allows test methods to not require public modifiers. 
+ * 
+ * Annotation @Autowired 
+ * -> Dependency Injection performed by Spring.
  */
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class HelloWorldControllerTest {
-    @Autowired  //Dependency Injection 
+class HelloWorldControllerTest {
+    @Autowired
     private MockMvc mockMvc;
 
-    @Test   
-    public void shouldReturnExpectedMessage() throws Exception {
+    @Test
+    void shouldReturnExpectedMessage() throws Exception {
         mockMvc.perform(get("/api/hello")).andExpect(status().isOk()).andExpect(content().string("Hello World!"));
     }
 }

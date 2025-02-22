450996620254fcceb9e3b71e57ced4982d094791
nathan00pdl <nathanpaiva00@gmail.com>
2025-02-22 16:37:38 -0300

    add controller

---
+++ src/main/java/com/nathan/hello_world/controller/HelloWorldController.java
@@ -0,0 +1,11 @@
+package com.nathan.hello_world.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldController {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldController.class, args);
+    }
+}
